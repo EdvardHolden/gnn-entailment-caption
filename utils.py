89bf90ab06f19d2bc4d1349c1bb02c24a3a6a6bd
EdvardHolden <edvardholden@gmail.com>
2022-01-07 16:49:44 +0100

    Add the utils

---
+++ utils.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+import re
+import os
+
+CLAUSE_PATTERN = b"((cnf|fof|tff)\\((.*\n)*?.*\\)\\.$)"
+
+
+def _get_clauses(problem_dir, problem):
+    # Read the problem file
+    path = os.path.join(problem_dir, problem.strip())
+    with open(path, "rb") as f:
+        text = f.read()
+
+    # Extract all axioms and extract the axiom group
+    res = re.findall(CLAUSE_PATTERN, text, re.MULTILINE)
+    res = [r[0] for r in res]
+
+    # Convert all cnf, tff clauses to fof (will remove type information later)
+    for n in range(len(res)):
+        res[n] = res[n].replace(b"cnf(", b"fof(")
+        res[n] = res[n].replace(b"tff(", b"fof(")
+
+    return res
+
+
+def _split_clauses(clauses):
+    # Filter all types from tff!
+    axioms = []
+    conjecture = []
+
+    for clause in clauses:
+        if b"conjecture" in clause:
+            conjecture += [clause]
+        elif b"type" in clause:
+            pass  # We discard tff types
+        else:
+            axioms += [clause]
+
+    return conjecture, axioms
+
+
+def read_problem_tptp(problem, problem_dir):
+
+    # Extract the clauses from the problem
+    clauses = _get_clauses(problem_dir, problem)
+
+    # Set first axiom to be the conjecture
+    conjecture, axioms = _split_clauses(clauses)
+
+    return conjecture, axioms
+
+
+def read_problem_deepmath(problem, root):
+    path = Path(root) / "nndata" / problem.strip()
+    with open(path, "rb") as f:
+        conjecture = next(f).strip()[2:]
+
+        premises, targets = zip(
+            *[
+                (
+                    line[2:],
+                    1.0 if line.startswith(b"+") else 0.0,
+                )
+                for line in f
+            ]
+        )
+
+    return [conjecture], premises, targets
