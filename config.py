40c36ea43993a95fc82ef4593e909151d02e3873
EdvardHolden <edvardholden@gmail.com>
2022-11-22 23:29:03 +0100

    Add node type constant

--- config.py
+++ config.py
@@ -6,3 +6,22 @@ device = "cpu"
 
 BENCHMARK_PATHS = {"deepmath": Path(__file__).parent / "nndata"}
 BATCH_SIZE = 64
+
+
+NODE_TYPE = {
+    0: "True",
+    1: "False",
+    2: "Variable",
+    3: "Functor",
+    4: "Argument",
+    5: "Application",
+    6: "Equality",
+    7: "Negation",
+    8: "And",
+    9: "Or",
+    10: "Equivalent",
+    11: "Forall",
+    12: "Exists",
+    13: "Axiom",
+    14: "Conjecture",
+}
