565923005cdb351d2d9961177b1115753519b799
EdvardHolden <edvardholden@gmail.com>
2022-12-29 15:31:14 +0100

    Fix with current dataset setup

--- train.py
+++ train.py
@@ -5,12 +5,25 @@ from torch.nn.functional import binary_cross_entropy_with_logits
 from torch.optim import SGD
 from torch.optim.lr_scheduler import CyclicLR
 from tqdm import tqdm
+import argparse
 
-from dataset import get_data_loader
+from dataset import get_data_loader, BenchmarkType
 from model import Model
 from statistics import Writer
 
 import config
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--train_id", default="id_files/train.txt", help="ID file used for training")
+    parser.add_argument("--val_id", default="id_files/validation.txt", help="ID file used for validation")
+    parser.add_argument(
+        "--benchmark_type", default="deepmath", type=BenchmarkType, help="Benchmark type fo the problems."
+    )
+
+    return parser
 
 
 def batch_loss(model, batch):
@@ -18,7 +31,7 @@ def batch_loss(model, batch):
     y = model(batch)
     loss = binary_cross_entropy_with_logits(y, batch.y)
     del batch
-    return (y, loss)
+    return y, loss
 
 
 def validation_loss(model, validation):
@@ -31,10 +44,16 @@ def validation_loss(model, validation):
     return torch.tensor(losses).mean()
 
 
-def train():
-    train = get_data_loader(Path(__file__).parent, "train.txt")
-    validation = get_data_loader(Path(__file__).parent, "validation.txt")
+def main():
 
+    # Get arguments
+    parser = get_parser()
+    args = parser.parse_args()
+
+    train_data = get_data_loader(args.train_id, args.benchmark_type)
+    val_data = get_data_loader(args.val_id, args.benchmark_type)
+
+    # TODO what is this number? 17
     model = Model(17).to(config.device)
     optimizer = SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4, nesterov=True)
     scheduler = CyclicLR(optimizer, 0.01, 0.1, mode="exp_range", gamma=0.99995, step_size_up=4000)
@@ -42,12 +61,18 @@ def train():
     stats = Writer(model)
     best_loss = torch.tensor(float("inf"))
 
-    while True:
+    #  TODO Separate the train and validation loop out?
+
+    # TOdO need better storing of training metrics
+
+    # TODO change to epochs?
+    # while True:
+    for _ in range(2):
         stats.report_model_parameters()
 
         print("validating...")
         model.eval()
-        val_loss = validation_loss(model, validation)
+        val_loss = validation_loss(model, val_data)
         stats.report_validation_loss(val_loss)
         if val_loss < best_loss:
             torch.save(model.state_dict(), "model_gnn.pt")
@@ -55,7 +80,7 @@ def train():
         print(f"...done, loss {val_loss:.3E}")
 
         model.train()
-        for batch in tqdm(train):
+        for batch in tqdm(train_data):
             optimizer.zero_grad()
             y, loss = batch_loss(model, batch)
             loss.backward()
@@ -70,7 +95,9 @@ def train():
 
 if __name__ == "__main__":
     torch.manual_seed(0)
+
+    # TODO move to training? - should be removed completely with epochs
     try:
-        train()
+        main()
     except KeyboardInterrupt:
         pass
