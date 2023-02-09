fdefeeeca1bd9538f53a769cd4469e2245728a86
Edvard Holden <edvard.holden@student.manchester.ac.uk>
2023-02-09 23:24:55 +0000

    Add nohup support and more

--- run_all_nodes.py
+++ run_all_nodes.py
@@ -1,9 +1,10 @@
-import sys
 import subprocess
 import argparse
 import time
 
 # Useful cmd# killall -u holden
+
+NOHUP_POST = " 2>&1 >> nohup_{0}.out & "
 
 
 def main():
@@ -13,18 +14,34 @@ def main():
     parser.add_argument("--timeout", type=int, default=30, help="Time out at node")
     parser.add_argument("--node_lbound", type=int, default=2, help="Lower node id")
     parser.add_argument("--node_ubound", type=int, default=45, help="Upper node id")
+    parser.add_argument(
+        "--nohup_post",
+        action="store_true",
+        help="Appends nohup stuff to the end of cmd",
+    )
     parser.add_argument("cmd", help="The command to run", nargs="+")
     args = parser.parse_args()
 
     cmd = " ".join(args.cmd)
-    print("Running cmd:", cmd)
+    if args.nohup_post:
+        if "nohup" not in cmd:
+            raise ValueError("Forgot to call nohup at front of cmd")
+        cmd += NOHUP_POST
+    print(f'Running cmd: "{cmd}"')
 
     for i in range(args.node_lbound, args.node_ubound):
         node = f"cc{i:02}"
 
         try:
+
+            if args.nohup_post:
+                cmd = cmd.format(i)
+
             proc = subprocess.Popen(
-                f"ssh {node} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                f"ssh {node} ' {cmd} '",
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
             )
             outs, errs = proc.communicate(timeout=args.timeout)
         except subprocess.TimeoutExpired:
@@ -35,6 +52,7 @@ def main():
         if errs != b"":
             print(f"Error on node {node} msg:", errs)
         time.sleep(args.delay)
+        print(node, outs)
 
     print("# Finished")
 
