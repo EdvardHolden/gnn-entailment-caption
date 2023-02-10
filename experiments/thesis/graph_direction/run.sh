aa1dbc99da937ec18fee29e0bde45dd1e8b218cd
EdvardHolden <edvardholden@gmail.com>
2023-02-10 20:04:10 +0100

    Add correct run files

--- experiments/thesis/graph_direction/run.sh
+++ experiments/thesis/graph_direction/run.sh
@@ -1,6 +1,6 @@
-python3 -u train.py --learning_task premise --experiment_dir experiments/thesis/graph_direction/premise
-python3 -u train.py --learning_task premise --experiment_dir experiments/thesis/graph_direction/premise --graph_bidirectional
-python3 -u train.py --learning_task similarity --experiment_dir experiments/thesis/graph_direction/similarity
-python3 -u train.py --learning_task similarity --experiment_dir experiments/thesis/graph_direction/similarityn --graph_bidirectional
+python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_direction/params.json --learning_task premise --experiment_dir experiments/thesis/graph_direction/premise
+python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_direction/params.json --learning_task premise --experiment_dir experiments/thesis/graph_direction/premise --graph_bidirectional
+python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_direction/params.json --learning_task similarity --experiment_dir experiments/thesis/graph_direction/similarity
+python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_direction/params.json --learning_task similarity --experiment_dir experiments/thesis/graph_direction/similarityn --graph_bidirectional
 
 python3 synthesise_results.py experiments/thesis/
