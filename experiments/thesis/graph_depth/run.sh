30a727731287892f4c277bd4734dae10ac1c8399
Edvard Holden <edvardholden@gmail.com>
2023-02-16 18:42:13 +0000

    Partially update exp 2 run scripts

--- experiments/thesis/graph_depth/run.sh
+++ experiments/thesis/graph_depth/run.sh
@@ -1,6 +1,4 @@
 python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_depth/params.json --learning_task premise --experiment_dir experiments/thesis/graph_depth/premise
-python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_depth/params.json --learning_task premise --experiment_dir experiments/thesis/graph_depth/premise_rm --graph_remove_argument_node
 python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_depth/params.json --learning_task similarity --experiment_dir experiments/thesis/graph_depth/similarity
-python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_depth/params.json --learning_task similarity --experiment_dir experiments/thesis/graph_depth/similarity_rm --graph_remove_argument_node
 
 #python3 synthesise_results.py experiments/thesis/
