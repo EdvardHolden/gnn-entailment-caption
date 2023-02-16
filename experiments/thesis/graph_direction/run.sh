84ea3f1af40229bb9514c061311c811e82480f05
Edvard Holden <edvardholden@gmail.com>
2023-02-16 16:07:24 +0000

    Update exp 1 config

--- experiments/thesis/graph_direction/run.sh
+++ experiments/thesis/graph_direction/run.sh
@@ -1,7 +1,4 @@
 python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_direction/params.json --learning_task premise --experiment_dir experiments/thesis/graph_direction/premise
-python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_direction/params.json --learning_task premise --experiment_dir experiments/thesis/graph_direction/premise_bi --graph_bidirectional
 python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_direction/params.json --learning_task similarity --experiment_dir experiments/thesis/graph_direction/similarity
-python3 -u search_hyperparams.py --parameter_space experiments/thesis/graph_direction/params.json --learning_task similarity --experiment_dir experiments/thesis/graph_direction/similarity_bi --graph_bidirectional
 
-python3 synthesise_results.py experiments/thesis/graph_direction/premise*
-python3 synthesise_results.py experiments/thesis/graph_direction/similarity*
+python3 synthesise_results.py experiments/thesis/graph_direction/
